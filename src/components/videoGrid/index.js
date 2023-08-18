5d6738b9a13054f358ce13e4006c624f4e157f12
Jonathan Maia <jonathancmaia@gmail.com>
2023-08-17 22:57:12 -0300

    added https mode on/off

--- src/components/videoGrid/index.js
+++ src/components/videoGrid/index.js
@@ -1,50 +1,51 @@
-import React, {useEffect, useContext} from 'react';
-import { Peer } from 'peerjs';
-import { IoContext } from '../room/index.js';
-import { style } from './style.css';
-import Enviroment from '../../config.js';
+import React, { useEffect, useContext } from "react";
+import { Peer } from "peerjs";
+import { IoContext } from "../room/index.js";
+import { style } from "./style.css";
+import Enviroment from "../../config.js";
 
-function VideoGrid(props){
-
+function VideoGrid(props) {
   const user = props.user;
   const userList = props.userList;
   let mediaHandler = null;
 
   const socket = useContext(IoContext);
 
-  useEffect(()=>{
+  useEffect(() => {
     //Peerjs setup
     const peer = new Peer(user, {
       host: Enviroment.peerServer,
       port: 9000,
-      path: '/',
-      secure: true,
-      debug: true
+      path: "/",
+      secure: Enviroment.https,
+      debug: true,
     });
 
     //Start videos events
-    document.addEventListener('addedVideo', (e)=>{
+    document.addEventListener("addedVideo", (e) => {
       let userVideo = e.detail.user;
-      let video = document.getElementById(e.detail.user).getElementsByTagName('video')[0];
+      let video = document
+        .getElementById(e.detail.user)
+        .getElementsByTagName("video")[0];
       video.muted = true;
       video.play();
 
       //Video vollume control creation
-      if (e.detail.user !== user){
-        let vollumeController = document.createElement('span');
-        vollumeController.setAttribute('class', 'vollumeController');
+      if (e.detail.user !== user) {
+        let vollumeController = document.createElement("span");
+        vollumeController.setAttribute("class", "vollumeController");
         document.getElementById(e.detail.user).append(vollumeController);
-        let vollumeRange = document.createElement('input');
-        vollumeRange.setAttribute('type', 'range');
-        vollumeRange.setAttribute('class', 'vollumeRange');
-        vollumeRange.setAttribute('min', '0');
-        vollumeRange.setAttribute('max', '100');
-        vollumeRange.setAttribute('value', '0');
-        vollumeRange.setAttribute('orient', 'vertical');
+        let vollumeRange = document.createElement("input");
+        vollumeRange.setAttribute("type", "range");
+        vollumeRange.setAttribute("class", "vollumeRange");
+        vollumeRange.setAttribute("min", "0");
+        vollumeRange.setAttribute("max", "100");
+        vollumeRange.setAttribute("value", "0");
+        vollumeRange.setAttribute("orient", "vertical");
 
         vollumeController.append(vollumeRange);
-        vollumeController.addEventListener('change', (e)=>{
-          if (video.muted){
+        vollumeController.addEventListener("change", (e) => {
+          if (video.muted) {
             video.muted = false;
             video.volume = parseFloat(vollumeRange.value / 100);
           } else {
@@ -55,108 +56,111 @@ function VideoGrid(props){
     });
 
     //Set user media
-    navigator.mediaDevices.getUserMedia({
-      audio: true,
-      video: {
-        width: 300,
-        height: 300,
-        facingMode: 'user'
-      }
-    }).then( stream => {
-      addVideo(stream, user);
-      mediaHandler = stream;
-
-      for (let i = 0; i < userList.length; i++){
-        if (userList[i] !== user){
-          let call = peer.call(userList[i], stream);
-          
-          call.on('stream', function(stream) {
-            if (document.getElementById(userList[i]) === null){
-              addVideo(stream, userList[i]);
-            }
-          });
-        }
-      }
-    }).catch(function() {
-
-      //USER HAVENT CAMERA
-      navigator.mediaDevices.getUserMedia({
+    navigator.mediaDevices
+      .getUserMedia({
         audio: true,
-        video: false
-      }).then( stream => {
+        video: {
+          width: 300,
+          height: 300,
+          facingMode: "user",
+        },
+      })
+      .then((stream) => {
         addVideo(stream, user);
         mediaHandler = stream;
-  
-        for (let i = 0; i < userList.length; i++){
-          if (userList[i] !== user){
+
+        for (let i = 0; i < userList.length; i++) {
+          if (userList[i] !== user) {
             let call = peer.call(userList[i], stream);
-            
-            call.on('stream', function(stream) {
-              if (document.getElementById(userList[i]) === null){
+
+            call.on("stream", function(stream) {
+              if (document.getElementById(userList[i]) === null) {
                 addVideo(stream, userList[i]);
               }
             });
           }
         }
-      }).catch(function() {
+      })
+      .catch(function() {
+        //USER HAVENT CAMERA
+        navigator.mediaDevices
+          .getUserMedia({
+            audio: true,
+            video: false,
+          })
+          .then((stream) => {
+            addVideo(stream, user);
+            mediaHandler = stream;
 
-        //CLIENT HAVENT CAMERA AND MICROPHONE
-        for (let i = 0; i < userList.length; i++){
-          if (userList[i] !== user){
-            peer.connect(userList[i]);
-          }
-        }
+            for (let i = 0; i < userList.length; i++) {
+              if (userList[i] !== user) {
+                let call = peer.call(userList[i], stream);
+
+                call.on("stream", function(stream) {
+                  if (document.getElementById(userList[i]) === null) {
+                    addVideo(stream, userList[i]);
+                  }
+                });
+              }
+            }
+          })
+          .catch(function() {
+            //CLIENT HAVENT CAMERA AND MICROPHONE
+            for (let i = 0; i < userList.length; i++) {
+              if (userList[i] !== user) {
+                peer.connect(userList[i]);
+              }
+            }
+          });
       });
-    });
 
-    peer.on('call', function (call) {
+    peer.on("call", function(call) {
       call.answer(mediaHandler);
 
-      call.on('stream', function (stream) {
-        if (document.getElementById(call.peer) === null){
+      call.on("stream", function(stream) {
+        if (document.getElementById(call.peer) === null) {
           addVideo(stream, call.peer);
         }
       });
     });
 
     //Call to a non video/audio user
-    peer.on('connection', function(conn) {
-      if (mediaHandler !== null){
+    peer.on("connection", function(conn) {
+      if (mediaHandler !== null) {
         peer.call(conn.peer, mediaHandler);
       }
     });
 
-    function addVideo(stream, id){
-
+    function addVideo(stream, id) {
       //Video container creation
-      let videoContainer = document.createElement('div');
-      videoContainer.setAttribute('class', 'videoContainer');
-      let userIdSpan = document.createElement('span');
+      let videoContainer = document.createElement("div");
+      videoContainer.setAttribute("class", "videoContainer");
+      let userIdSpan = document.createElement("span");
       userIdSpan.textContent = id;
-      userIdSpan.setAttribute('class', 'videoId');
-      videoContainer.setAttribute('id', id);
-      document.getElementById('videoGrid').append(videoContainer);
+      userIdSpan.setAttribute("class", "videoId");
+      videoContainer.setAttribute("id", id);
+      document.getElementById("videoGrid").append(videoContainer);
       document.getElementById(id).append(userIdSpan);
 
       //Video element creation
-      let video = document.createElement('video');
+      let video = document.createElement("video");
 
       try {
-        video.setAttribute('width', '250');
-        video.setAttribute('height', '250');
+        video.setAttribute("width", "250");
+        video.setAttribute("height", "250");
         video.srcObject = stream;
       } catch (err) {
         console.log(err);
       }
 
-      if (id === user){
+      if (id === user) {
         video.muted = true;
       }
 
       document.getElementById(id).append(video);
 
       //Start video event
-      let addedVideo = new CustomEvent('addedVideo', { detail: {user: id} });
+      let addedVideo = new CustomEvent("addedVideo", { detail: { user: id } });
       document.dispatchEvent(addedVideo);
 
       //Create an AudioContext and AnalyserNode for the video stream
@@ -183,27 +187,24 @@ function VideoGrid(props){
       setInterval(() => {
         const dbLevel = calculateDbLevel();
         if (dbLevel > THRESHOLD_DB) {
-          video.classList.add('sound-active');
-          console.log(dbLevel)
+          video.classList.add("sound-active");
+          console.log(dbLevel);
         } else {
-          video.classList.remove('sound-active');
-          console.log(dbLevel)
+          video.classList.remove("sound-active");
+          console.log(dbLevel);
         }
       }, INTERVAL_MS);
     }
 
     //Socket event
-    socket.on('removeVideo',(id)=>{
-      if (document.getElementById(id) !== null){
+    socket.on("removeVideo", (id) => {
+      if (document.getElementById(id) !== null) {
         document.getElementById(id).remove();
       }
     });
-  },[]);
+  }, []);
 
-  return(
-    <div id='videoGrid'>
-    </div>
-  );
-};
+  return <div id="videoGrid"></div>;
+}
 
 export default VideoGrid;
--- src/config.js
+++ src/config.js
@@ -2,13 +2,16 @@ const dev = false;
 
 let socketServer = "";
 let peerServer = "";
+let https = "";
 
 if (dev) {
   socketServer = "http://localhost:7000";
   peerServer = "localhost";
+  https = false;
 } else {
   socketServer = "http://ec2-3-12-148-178.us-east-2.compute.amazonaws.com:7000";
   peerServer = "ec2-3-12-148-178.us-east-2.compute.amazonaws.com";
+  https = false;
 }
 
 export default {
